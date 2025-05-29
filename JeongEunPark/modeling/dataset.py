f4e869d72f90fc4e47b93aac4dfacc46839e10f9
jepark <gkwjdtk@naver.com>
2025-05-29 16:31:08 +0900

    feat: implement image captioning pipeline with KoGPT2 and ResNet50

--- JeongEunPark/modeling/dataset.py
+++ JeongEunPark/modeling/dataset.py
@@ -3,42 +3,59 @@ from torch.utils.data import Dataset
 from PIL import Image
 from torchvision.transforms import Resize, ToTensor, Normalize, Compose
 import os
+from transformers import PreTrainedTokenizerFast
 
 class CustomDataset(Dataset):
-    def __init__(self, img_filenames_list, captions_list, vocab, max_cap_length):
+    def __init__(self, img_filenames_list, captions_list, tokenizer, max_cap_length=82):
         super().__init__()
         self.img_filenames_list = img_filenames_list
         self.captions_list = captions_list
-        self.length = len(self.captions_list)
+        self.tokenizer = tokenizer
+        self.max_cap_length = max_cap_length
+        
+
+        # 이미지 전처리 정의
         self.transform = Compose([
             Resize((224, 224), antialias=True),
             ToTensor(),
-            Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            Normalize(mean=[0.485, 0.456, 0.406], 
+                    std=[0.229, 0.224, 0.225])
         ])
-        self.vocab = vocab
-        self.max_cap_length = max_cap_length
+        
 
     def __len__(self):
-        return self.length
+        return len(self.captions_list)
 
-    def get_input_ids(self, sentence):
-        input_ids = [0] * (self.max_cap_length + 2)
-        i = 1
-        for word in sentence.split(" "):
-            input_ids[i] = self.vocab.word2index.get(word, self.vocab.word2index['EOS'])
-            i += 1
-            if i >= self.max_cap_length + 1:
-                break
-        input_ids[0] = self.vocab.word2index['SOS']
-        input_ids[i] = self.vocab.word2index['EOS']
-        return torch.tensor(input_ids)
+    # def get_input_ids(self, sentence):
+    #     input_ids = [0] * (self.max_cap_length + 2)
+    #     i = 1
+    #     for word in sentence.split(" "):
+    #         input_ids[i] = self.vocab.word2index.get(word, self.vocab.word2index['EOS'])
+    #         i += 1
+    #         if i >= self.max_cap_length + 1:
+    #             break
+    #     input_ids[0] = self.vocab.word2index['SOS']
+    #     input_ids[i] = self.vocab.word2index['EOS']
+    #     return torch.tensor(input_ids)
 
     def __getitem__(self, idx):
         img_path = self.img_filenames_list[idx]
-        caption = self.captions_list[idx]
         try:
-            img = Image.open(img_path).convert('RGB')
+            image = Image.open(img_path).convert('RGB')
         except Exception:
-            img = Image.new("RGB", (224, 224))
-        img = self.transform(img)
-        caption_tensor = self.get_input_ids(caption)
+            image = Image.new("RGB", (224, 224))
+        image = self.transform(image)
+
+        caption = self.captions_list[idx]
+        caption_with_token = f"</s>{caption}</s>"
+        tokenized = self.tokenizer(
+            caption_with_token,
+            max_length = self.max_cap_length,
+            padding="max_length",
+            truncation=True,
+            return_tensors="pt"
+        )
+        input_ids = tokenized["input_ids"].squeeze(0)
+        attention_mask = tokenized["attention_mask"].squeeze(0)
+
+        return image, input_ids, attention_mask
--- JeongEunPark/modeling/main.py
+++ JeongEunPark/modeling/main.py
@@ -4,16 +4,19 @@ from torch.utils.data import DataLoader, random_split
 from pathlib import Path
 from preprocess import normalize_caption, Vocab
 from dataset import CustomDataset
-from modeling import EncoderCNN, DecoderRNN
+from model import EncoderCNN, DecoderRNN
 from train import train_model 
 from inference import generate_caption
 import json, os
+from torch import optim
+from transformers import PreTrainedTokenizerFast
 
 CSV_PATH = "/home/jepark/dev/Toon_Persona_eda/JeongEunPark/modeling/toon_caption.csv"
 BATCH_SIZE = 16
-MAX_SAMPLES = 100 # 불러올 데이터 개수
-NUM_EPOCHS = 1
+MAX_SAMPLES = 5000 # 불러올 데이터 개수
+NUM_EPOCHS = 5
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+TOKENIZER_NAME = "skt/kogpt2-base-v2"
 
 # Load data
 img_filenames_list = []
@@ -30,28 +33,34 @@ with open(CSV_PATH, 'r', encoding='utf-8') as f:
 
 print(f"총 {len(captions_list)}개 캡션 로드 완료")
 
-# vocab
-vocab = Vocab()
-for caption in captions_list:
-    vocab.build_vocab(caption)
+# # vocab
+# vocab = Vocab()
+# for caption in captions_list:
+#     vocab.build_vocab(caption)
 
-print(f"\nVocab 크기: {vocab.nwords}")
+# print(f"\nVocab 크기: {vocab.nwords}")
 
 # max caption length
 max_cap_length = max(len(c.split(" ")) for c in captions_list)
 
 print(f"최대 캡션 길이: {max_cap_length}")
 
+# tokenizer = PreTrainedTokenizerFast.from_pretrained(TOKENIZER_NAME)
+tokenizer = PreTrainedTokenizerFast.from_pretrained(TOKENIZER_NAME,
+  bos_token='</s>', eos_token='</s>', unk_token='<unk>',
+  pad_token='<pad>', mask_token='<mask>')
+
+
 # DataLoader
 full_dataset = CustomDataset(
     img_filenames_list=img_filenames_list,
     captions_list=captions_list,
-    vocab=vocab,
+    tokenizer=tokenizer,
     max_cap_length=max_cap_length
 )
 
 train_dataset, test_dataset = random_split(full_dataset, [0.9, 0.1])
-train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True) # 여러 개의 샘플을 모아서 미니 배치 생성
 test_dataloader = DataLoader(test_dataset, batch_size=1)
 
 print("전체 데이터 수:", len(full_dataset))
@@ -59,40 +68,46 @@ print("학습 데이터 수:", len(train_dataset))
 print("테스트 데이터 수:", len(test_dataset))
 
 EMBED_SIZE = 256
-HIDDEN_SIZE = 512
-VOCAB_SIZE = vocab.nwords
+# HIDDEN_SIZE = 512
+# VOCAB_SIZE = vocab.nwords
 
 encoder = EncoderCNN(EMBED_SIZE).to(DEVICE)
-decoder = DecoderRNN(EMBED_SIZE, HIDDEN_SIZE, VOCAB_SIZE).to(DEVICE)
+decoder = DecoderRNN(EMBED_SIZE, model_name=TOKENIZER_NAME).to(DEVICE)
 
 print("모델 초기화 완료")
 print(f"Encoder: {encoder.__class__.__name__}")
 print(f"Decoder: {decoder.__class__.__name__}")
 
+optimizer = optim.AdamW(
+    list(encoder.parameters()) + list(decoder.parameters()),
+    lr=5e-5,
+    weight_decay=0.01 # 모델이 학습 데이터에만 너무 잘 맞고, 테스트 성능이 떨어질 때 넣어주는게 안전
+)
+
 train_model(
     encoder=encoder,
     decoder=decoder,
-    train_loader=train_dataloader,
-    vocab_size=VOCAB_SIZE,
+    dataloader=train_dataloader,
+    optimizer = optimizer,
     device=DEVICE,
     num_epochs=NUM_EPOCHS,
-    learning_rate=1e-3
+    # learning_rate=1e-3 
 )
 
-# 모델 저장
-if not os.path.exists("state_dict"):
-    os.makedirs("state_dict")
-torch.save(encoder.state_dict(), "state_dict/encoder_epoch_0.pt")
-torch.save(decoder.state_dict(), "state_dict/decoder_epoch_0.pt")
+# # 모델 저장
+# if not os.path.exists("state_dict"):
+#     os.makedirs("state_dict")
+# torch.save(encoder.state_dict(), "state_dict/encoder_epoch_0.pt")
+# torch.save(decoder.state_dict(), "state_dict/decoder_epoch_0.pt")
 
 print("\n 추론 결과 (샘플 3개):")
 for i in range(3):  # 예시로 3개 추론
     img_path = img_filenames_list[i]
     caption = generate_caption(
         image_path=img_path,
-        encoder_path="state_dict/encoder_epoch_0.pt",
-        decoder_path="state_dict/decoder_epoch_0.pt",
-        vocab=vocab,
+        encoder=encoder,
+        decoder=decoder,
+        tokenizer=tokenizer,
         device=DEVICE
     )
     print(f"\n이미지: {img_path}")
